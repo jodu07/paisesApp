6e82d79e044c77f6428425c8a85cfeee654a6d32
jodu07 <jhonnat.dussan@gmail.com>
2022-02-24 16:49:44 -0500

    commit 1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PorCapitalComponent } from './pais/pages/por-capital/por-capital.component';
+import { PorPaisComponent } from "./pais/pages/por-pais/por-pais.component";
+import { PorRegionComponent } from "./pais/pages/por-region/por-region.component";
+import { VerPaisComponent } from "./pais/pages/ver-pais/ver-pais.component";
+
+
+
+
+
+const routes: Routes = [
+
+    //pathMatch me dirige a esta pagina cuando es vacio, queda como la principal
+    {path:'' , component: PorPaisComponent, pathMatch:'full'}, 
+    
+    {path:'capital' , component: PorCapitalComponent},    
+    {path:'region' , component: PorRegionComponent},
+    {path:'pais/:codigoPais' , component: VerPaisComponent},
+
+    //definir una ruta en caso de que el usuario digite una ruta que no este defnida se redirige a la principal
+    {path: '**', redirectTo: ''}
+   
+  
+  ];
+  
+  @NgModule({
+    imports: 
+    [
+        RouterModule.forRoot(routes)
+    ],
+    exports: 
+    [
+        RouterModule
+    ]
+  })
+  export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+
 import { AppComponent } from './app.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PaisModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
